8e01c25431c75d71a4b0e2bc901a0b534cd2d602
aleksandra_tokareva <nimda1986@gmail.com>
2022-02-28 17:08:50 -0600

    add style for profile and places

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+// //get all relevant elemenets from the DOM
+// const modalWindow = document.querySelector('.popup');
+// const editForm = document.querySelector('.popup__form');
+
+// const editButton= document.querySelector('.profile__edit_button');
+// const closeButton=document.querySelector('.popup__close');
+// const profileTitle=document.querySelector('.profile__title');
+// const profileDescription=document.querySelector('.profile__description');
+
+// const titleInputField= editForm.querySelector('.popup__input_type_name');
+// const descriptionInputField= editForm.querySelector('.popup__input_type_description')
+
+// //write function to toggle modal visibility and submit button
+// function toggleModalVisibility (){
+//   if(!modalWindow.classList.contains('popup__is_opened')){
+//       titleInputField.value=profileTitle.textContent;
+//       descriptionInputField.value=profileDescription.textContent;
+//   }
+// modalWindow.classList.toggle('popup__is_opened');
+// }
+
+// function formSubmitHandler (evt) {
+//     evt.preventDefault();
+
+//     profileTitle.textContent=titleInputField.value;
+//     profileDescription.textContent=descriptionInputField.value;
+
+//     toggleModalVisibility();
+// }
+// //wiring the function from step2 to the appropriate elements from step1  
+
+// editForm.addEventListener('submit', formSubmitHandler);
+// editButton.addEventListener('click', formSubmitHandler);
+
+const clickOnEditProfileButton=document.querySelector('.profile__edit-button');
+
+clickOnEditProfileButton.addEventListener('click', function(event) {
+console.dir(event)
+});
